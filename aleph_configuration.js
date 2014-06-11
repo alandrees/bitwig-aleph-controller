991364126f9fe69019dfa3db7792f8f94923d25d
Alan Drees <alandrees@theselves.com>
2014-06-10 22:10:45 -0400

    example aleph configuration file

---
+++ aleph_configuration.js
@@ -0,0 +1,17 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *   Configuration file for the Aleph script system
+ */
+
+var Aleph = Aleph || {};
+
+Aleph.controller_list = [];
+
+/*
+This is an example of how you would specify other controllers to load into the Aleph environment
+*/
+Aleph.controller_list.push('Launchpad');
+Aleph.controller_list.push('BCR2000');
+Aleph.controller_list.push('BCF2000');
