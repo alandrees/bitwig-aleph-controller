1af8b84bf1299f9abcb3bc509e625c034fcd809b
Alan Drees <alandrees@theselves.com>
2014-06-10 22:10:16 -0400

    inital commit for the script entry point

---
+++ Aleph.control.js
@@ -0,0 +1,39 @@
+loadAPI(1);
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *   Script entry point for the aleph script encapsulator
+ *
+ * Requires:
+ */
+
+var Aleph = Aleph || {};
+
+/************/
+/* requires */
+/************/
+
+load('aleph_core.js');
+
+/*************/
+/* functions */
+/*************/
+
+/**\fn init
+ * 
+ * Entry point for the program.  Calls the Aleph.init function.
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+function init(){
+    if(typeof Aleph !== undefined)
+    {
+	Aleph.init();
+    }
+}
+
+init();
