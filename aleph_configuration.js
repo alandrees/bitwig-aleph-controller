31e3d459de807e8ea43d6d8346e58e1f709b5d47
Alan Drees <alandrees@theselves.com>
2014-06-12 01:45:40 -0400

    added some more comments, added io requirements per script to be loaded

--- aleph_configuration.js
+++ aleph_configuration.js
@@ -2,7 +2,11 @@
  * Copyright 2014 Alan Drees
  *   
  * Purpose:
- *   Configuration file for the Aleph script system
+ *  Configuration file for the Aleph script system.  In lieu of 
+ *
+ * Requires:
+ *  None
+ *
  */
 
 var Aleph = Aleph || {};
@@ -12,6 +16,14 @@ Aleph.controller_list = [];
 /*
 This is an example of how you would specify other controllers to load into the Aleph environment
 */
-Aleph.controller_list.push('Launchpad');
-Aleph.controller_list.push('BCR2000');
-Aleph.controller_list.push('BCF2000');
+
+/*
+0 -> name of the controller to load the script for
+1 -> number of inputs total
+2 -> number of outputs total
+3 -> number of controllers total
+*/
+
+Aleph.controller_list.push(['Launchpad', 1, 1, 1] );
+Aleph.controller_list.push(['BCR2000', 1, 1, 1]);
+Aleph.controller_list.push(['BCF2000', 1, 1, 1]);
